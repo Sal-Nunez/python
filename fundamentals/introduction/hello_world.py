35b81a51ade1eba70d3e49eaf76ebf3792afe199
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-08-28 13:00:26 -0700

    pythonFundamentals

--- fundamentals/introduction/hello_world.py
+++ fundamentals/introduction/hello_world.py
@@ -1,6 +1,6 @@
-# name = "Sal Nuñez"
-# favNum = 17
-# print("Hello " + f"{favNum}")
+name = "Sal Nuñez"
+favNum = 17
+print("Hello " + f"{favNum}")
 favFood = ("Pizza", "Hamburgers")
-# print("I love to eat " + str(favFood[0] + " and " + str(favFood[1])))
+print("I love to eat " + str(favFood[0] + " and " + str(favFood[1])))
 print(f"I love to eat {favFood[0]} and {favFood[1]}")
