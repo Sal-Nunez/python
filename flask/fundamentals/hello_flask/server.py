59ecc688c00b8d9ca08cced315953c3701fc59fb
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-09-01 20:43:21 -0700

    playground

--- flask/fundamentals/hello_flask/server.py
+++ flask/fundamentals/hello_flask/server.py
@@ -4,7 +4,7 @@ from flask import render_template
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return render_template("index.html")  # Return the string 'Hello World!' as a response
 
 @app.route('/success')
 def success():
@@ -14,15 +14,16 @@ def success():
 @app.errorhandler(404)
 def page_not_found(e):
     # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
+    return render_template('404.html')
 
 @app.route('/dojo')
 def dojo():
     return "Dojo!"
 
+@app.route('/hello/<string:banana>/<int:num>')
 @app.route('/hello/<banana>')
-def hello(banana):
-    return f"Hello {banana}"
+def hello1(banana, num):
+    return render_template('hello.html', banana=banana, num=num)
 
 @app.route('/say/<string:banana>')
 def say(banana):
@@ -34,9 +35,12 @@ def show_user_profile(username, id):
     print(id)
     return "username: " + username + ", id: " + id
 
-@app.route('/hello1/<string:banana>/<int:num>')
-def hello1(banana, num):
-    return f"Hello {banana * num}"
+
+@app.route('/play/<int:num>/<color>')
+@app.route('/play/<int:num>')
+@app.route('/play')
+def play(num=5, color='teal'):
+    return render_template('bboxes.html', num=num, color=color)
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
